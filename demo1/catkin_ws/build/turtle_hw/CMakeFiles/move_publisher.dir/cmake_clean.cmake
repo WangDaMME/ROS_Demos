9274f94c4fca926545be5e5a0cea48877b872a2e
wangde.mme <wangde.mme@gmail.com>
2020-01-02 20:59:30 -0500

    1st submission

---
+++ demo1/catkin_ws/build/turtle_hw/CMakeFiles/move_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/move_publisher.dir/src/move_publisher.cpp.o"
+  "/home/robond/workspace3/catkin_ws/devel/lib/turtle_hw/move_publisher.pdb"
+  "/home/robond/workspace3/catkin_ws/devel/lib/turtle_hw/move_publisher"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/move_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
