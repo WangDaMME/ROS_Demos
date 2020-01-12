f0703f23c22be90d421216c6c1bac06a71be4eef
wangda.mme <wangda.mme@gmail.com>
2020-01-12 17:55:10 -0500

    delete

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wangda/catkin_ws_voice/src
-# Build directory: /home/wangda/catkin_ws_voice/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(robot_voice)
--- build/robot_voice/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wangda/catkin_ws_voice/src/robot_voice
-# Build directory: /home/wangda/catkin_ws_voice/build/robot_voice
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
