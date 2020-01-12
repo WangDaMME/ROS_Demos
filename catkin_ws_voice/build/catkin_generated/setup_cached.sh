ca598f7b5adb62f7bd1e4206cf8179f43892ea6d
wangda.mme <wangda.mme@gmail.com>
2020-01-12 18:48:22 -0500

    submission

---
+++ catkin_ws_voice/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/wangda/catkin_ws_voice/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/wangda/catkin_ws_voice/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/wangda/catkin_ws_voice/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/wangda/catkin_ws_voice/src:$ROS_PACKAGE_PATH"
---
+++ catkin_ws_voice/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/wangda/catkin_ws_voice/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
